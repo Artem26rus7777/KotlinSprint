99cf7711912b55c00a362151d34f3cab876c8987
artem <artempopenya@gmail.com>
2024-06-06 12:17:37 +0300

    KS-1-1 create new variable and print to concole

---
+++ src/main/kotlin/lesson_1/lesson1-task3.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_1
+
+fun main() {
+    val year = 1961
+    var hour = "09"
+    var minute = "07"
+
+    println("Год полета Юрия Гагарина в космос - $year")
+    println("Взлет - $hour:$minute")
+
+    hour = "10"
+    minute = "55"
+    println("Время посадки - $hour:$minute")
+   
+}
