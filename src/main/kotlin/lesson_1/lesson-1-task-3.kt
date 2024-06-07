4c7cf7c1b40d034f4b3ddd21edf2ad08aa738b55
Artem26rus7777 <artempopenya@gmail.com>
2024-06-07 19:27:26 +0300

    KS-1-3 createe four variables and print to screen

---
+++ src/main/kotlin/lesson_1/lesson-1-task-3.kt
@@ -0,0 +1,13 @@
+package lesson_1
+
+fun main (){
+    val year = 1961
+    val takeOff = "09:07"
+    val hour = 10
+    val minute = 55
+
+    println("Год полета: $year")
+    println("Взлет: $takeOff")
+    println("Посадка: $hour:$minute")
+
+}
