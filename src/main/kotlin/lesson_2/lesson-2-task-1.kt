39d26601bc291ff03311f5a03743e8836af5fdd6
Artem26rus7777 <artempopenya@gmail.com>
2024-06-07 19:53:46 +0300

    KS-2-1 Calculated the grade point average

---
+++ src/main/kotlin/lesson_2/lesson-2-task-1.kt
@@ -0,0 +1,10 @@
+package lesson_2
+
+fun main (){
+    val englishStudent: Double = 4.0
+    val englishScore: Double = 3.0+4.0+3.0+5.0
+    val sumScore: Double = englishScore / englishStudent
+
+    println("Средний балл: $sumScore")
+
+}
