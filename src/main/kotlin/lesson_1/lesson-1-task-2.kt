9f436b35a92f57b582dca3cd26500ea9ca9de178
Artem26rus7777 <artempopenya@gmail.com>
2024-06-08 10:01:05 +0300

    KS-1-2 add variables from KS-1-1

--- src/main/kotlin/lesson_1/lesson-1-task-2.kt
+++ src/main/kotlin/lesson_1/lesson-1-task-2.kt
@@ -1,6 +1,10 @@
 package lesson_1
 
 fun main(){
+    val orders = 75
+    println("Количество заказов: $orders")
+    println("Спасибо за покупку")
+
     var workers = 2000
 //    println("Количество работников: $workers")
 
