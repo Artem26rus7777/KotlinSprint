665869aee0a6008fca3ec20561093b8478c0696c
Artem26rus7777 <artempopenya@gmail.com>
2024-06-08 10:09:12 +0300

    KS-1-4 fixed it

--- src/main/kotlin/lesson_1/lesson-1-task-4.kt
+++ src/main/kotlin/lesson_1/lesson-1-task-4.kt
@@ -1,12 +1,12 @@
 package lesson_1
 
 fun main () {
-    val length = "40_868_600_000"
+    val length: String = "40_868_600_000"
     val age: Byte = 27
     val partOfDay: Double = 0.075
     val seconds: Short = 6480
-    val partOfYear = "2.0547945205479453E-4"
-    val apogee = 327000
+    val partOfYear: String = "2.0547945205479453E-4"
+    val apogee: Int = 327000
 
     println("Расстояние: $length")
     println("Возраст: $age")
