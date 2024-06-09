353c603200721c989196222c904656a873905b3a
Artem26rus7777 <artempopenya@gmail.com>
2024-06-09 17:53:58 +0300

    KS-3-2

---
+++ src/main/kotlin/lesson_3/lesson-3-task-2.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_3
+
+fun main(){
+    val firstName = "Татьяна"
+    val middleName = "Сергеевна"
+    var lastName = "Андреева"
+    var age = 20
+
+    println("$lastName $firstName $middleName, $age")
+
+    lastName = "Сидорова"
+    age = 22
+
+    println("$lastName $firstName $middleName, $age")
+
+}
