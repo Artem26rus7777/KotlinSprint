b975decef1e9a7cc664a4ab8cdace9ba5162fbcd
Artem26rus7777 <artempopenya@gmail.com>
2024-06-10 17:56:53 +0300

    KS-2-3 fixed it

--- src/main/kotlin/lesson_2/lesson-2-task-3.kt
+++ src/main/kotlin/lesson_2/lesson-2-task-3.kt
@@ -4,13 +4,18 @@ fun main(){
     val hourDepartureTime = 9
     val minuteDepartureTime = 39
 
-    val totalTravelTime = 457
-    val hourTravelTime = totalTravelTime / 60
-    val minuteTravelTime = totalTravelTime % 60
+//    60 минут в одном часе
+    val minutesInOneHour = 60
 
-    val sum = (hourDepartureTime * 60) + minuteDepartureTime + totalTravelTime
-    val hourArrivalTime = sum / 60
-    val minuteArrivalTime = sum % 60
+    val totalTravelTime = 457
+    val hourTravelTime = totalTravelTime / minutesInOneHour
+    val minuteTravelTime = totalTravelTime % minutesInOneHour
+
+
+
+    val sum = (hourDepartureTime * minutesInOneHour) + minuteDepartureTime + totalTravelTime
+    val hourArrivalTime = sum / minutesInOneHour
+    val minuteArrivalTime = sum % minutesInOneHour
 
     println("Время выезда: $hourDepartureTime:$minuteDepartureTime")
     println("Время в пути: $hourTravelTime:$minuteTravelTime")
